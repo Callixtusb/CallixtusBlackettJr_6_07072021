a77d6544f69f4722f9b8e8b3b51a50b9cdb6db4c
Callixtus <callixtusb@gmail.com>
2021-07-27 13:10:44 +0200

    Re-initialisation du projet pour debugger la connexion a MongoDB

--- Back-end/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userCtrl = require('../controllers/user');
-const pwdValidator = require('../middleware/password');
-
-router.post('/signup', pwdValidator, userCtrl.signup);
-router.post('/login', userCtrl.login);
-
-module.exports = router;
